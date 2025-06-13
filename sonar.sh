05c2db08a205be816f708884e9d07ea8ad3b802e
bhavitharepository <reddyvenkatrami22@gmail.com>
2025-06-13 23:52:55 +0530

    Update sonar.sh

--- sonar.sh
+++ sonar.sh
@@ -6,9 +6,9 @@ unzip sonarqube-8.9.6.50800.zip
 amazon-linux-extras install java-openjdk11 -y
 useradd sonar
 chown sonar:sonar sonarqube-8.9.6.50800 -R
-chmod 777 sonarqube-8.9.6.50800 -R
+//chmod 777 sonarqube-8.9.6.50800 -R
 su - sonar
-
+==[/opt/sonarqube-8.9.6.50800/bin/linux-x86-64]
 #run this on server manually
 #sh /opt/sonarqube-8.9.6.50800/bin/linux/sonar.sh start
 #echo "user=admin & password=admin"
